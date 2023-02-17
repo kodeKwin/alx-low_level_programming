e19212b959aaf6068bbd62c7b9e899592b412ab5
kodekwin <queenmedinatolamide@gmail.com>
2023-02-16 16:06:52 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 /**
  *
- * main = entry point
+ * main = Entry point
  *
- * return: always 0 (success)
+ * return: Always 0 (Success)
  */
 
 int main(void)
 
-{printf("with proper grammer, but the outcome is a piece of art,\n");
+{printf("with proper grammer, but the outcome
+	       	is a piece of art,\n");
 
 	return (0);
 
